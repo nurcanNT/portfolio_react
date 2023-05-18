c0886fb3bfc4d8cfa22193b862a519aa7af6bf9d
nurcanNT <nrcntpkra@gmail.com>
2023-05-18 13:24:31 +0300

    pages and components update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
-
+import Navbar from './components/Navbar';
+import About from './pages/about/About';
+import Contact from './pages/contact/Contact';
+import Home from './pages/home/Home';
+import Portfolio from './pages/portfolio/Portfolio';
 function App() {
  
   return (
-    <>
-     
-    </>
-  )
+   <BrowserRouter>
+   <Navbar/>
+   <Routes>
+    <Route index element={<Home/>}/>
+    <Route path='about' element={<About/>}/>
+    <Route path='portfolio' element={<Portfolio/>}/>
+    <Route path='contact' element={<Contact/>}/>
+   </Routes>
+   </BrowserRouter>  
+   );
 }
 
 export default App
---
+++ src/pages/portfolio/Portfolio.jsx
@@ -0,0 +1,11 @@
+
+
+const Portfolio = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Portfolio
