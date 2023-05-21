fb18e26b084010bdd74072798d7de1bd8be5f180
nurcanNT <nrcntpkra@gmail.com>
2023-05-21 23:00:58 +0300

    about

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -3,6 +3,7 @@ const About = () => {
   return (
     <div>
       <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sunt voluptatem quaerat eos consequuntur praesentium, natus reiciendis facilis debitis voluptatum quos pariatur soluta nostrum eius id officiis incidunt nesciunt aut.</p>
     </div>
   )
 }
