c1884981c304ff34b78d08889d344f4954e19760
nurcanNT <nrcntpkra@gmail.com>
2023-05-25 01:11:09 +0300

    home update

---
+++ src/components/home/HeaderSocials.jsx
@@ -0,0 +1,29 @@
+//import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className="home__socials">
+        <a href="https://www.instagram.com/" className="home__social-link" target="_blank">
+            <i className="fa-brands fa-instagram"></i>
+        </a>
+
+        <a href="" className="home__social-link" target="_blank">
+            <i className="fa-brands fa-twitter"></i>
+        </a>
+
+        <a href="" className="home__social-link" target="_blank">
+            <i className="fa-brands fa-behance"></i>
+        </a>
+
+        <a href="" className="home__social-link" target="_blank">
+            <i className="fa-brands fa-dribbble"></i>
+        </a>
+
+        <a href="" className="home__social-link" target="_blank">
+            <i className="fa-brands fa-pinterest"></i>
+        </a>
+    </div>
+  )
+}
+
+export default HeaderSocials
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,19 @@
 //import React from 'react'
 import  "./home.css";
+import Me from "../../assets/avatar-1.svg";
+import HeaderSocials from "./HeaderSocials";
+
+
 const Home = () => {
   return (
-    <div>
-      Home
+   <section className="home container" id="home">
+    <div className="intro">
+        <img src={Me} alt="" className="home__img" />
+        <h1 className="home__name">Nurcan Topkara</h1>
+        <span className="home__education">I am a Front-End developer</span>
+    <HeaderSocials />
     </div>
+   </section>
   )
 }
 
