6752b17a3b11bf363d17524a327fc8dc2a8dc4d0
nurcanNT <nrcntpkra@gmail.com>
2023-05-26 02:00:11 +0300

    resume update new

---
+++ src/components/resume/Data.jsx
@@ -0,0 +1,53 @@
+//import React from 'react'
+
+const Data = [
+    {
+        id:1,
+        category: "education",
+        icon: "icon-graduation",
+        year: "2019 - present",
+        title: "Academic Degree",
+        desc: "Lorem ipsum color sit amet quo ei simul congue exerci ad nec admodum perfecto",
+    },
+    {
+        id:2,
+        category: "education",
+        icon: "icon-graduation",
+        year: "2019 - present",
+        title: "Bachelor's Degree",
+        desc: "Lorem ipsum color sit amet quo ei simul congue exerci ad nec admodum perfecto",
+    },
+    {
+        id:3,
+        category: "education",
+        icon: "icon-graduation",
+        year: "2019 - 2022",
+        title: "Academic Degree",
+        desc: "Lorem ipsum color sit amet quo ei simul congue exerci ad nec admodum perfecto",
+    },
+    {
+        id:4,
+        category: "education",
+        icon: "icon-graduation",
+        year: "2017 - present",
+        title: "Designer",
+        desc: "Lorem ipsum color sit amet quo ei simul congue exerci ad nec admodum perfecto",
+    },
+    {
+        id:5,
+        category: "education",
+        icon: "icon-graduation",
+        year: "2019 - 2020",
+        title: "Frontend Developer",
+        desc: "Lorem ipsum color sit amet quo ei simul congue exerci ad nec admodum perfecto",
+    },
+    {
+        id:6,
+        category: "experience",
+        icon: "icon-briefcase",
+        year: "2009 - 2013",
+        title: "Backend Developer",
+        desc: "Lorem ipsum color sit amet quo ei simul congue exerci ad nec admodum perfecto",
+    },
+];
+export default Data
