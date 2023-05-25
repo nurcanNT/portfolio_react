8b5859a9c9e58ec7f42ffbdf2133420c57da5f8c
nurcanNT <nrcntpkra@gmail.com>
2023-05-25 16:09:16 +0300

    some updates

--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -1,29 +1,29 @@
-//import React from 'react'
+//import React from 'react';
 
 const HeaderSocials = () => {
   return (
     <div className="home__socials">
-        <a href="https://www.instagram.com/" className="home__social-link" target="_blank">
-            <i className="fa-brands fa-instagram"></i>
-        </a>
+      <a href="https://www.instagram.com/" className="home__social-link" target="_blank" rel="noreferrer">
+        <i className="fa-brands fa-instagram"></i>
+      </a>
 
-        <a href="" className="home__social-link" target="_blank">
-            <i className="fa-brands fa-twitter"></i>
-        </a>
+      <a href="https://twitter.com/" className="home__social-link" target="_blank" rel="noreferrer">
+        <i className="fa-brands fa-twitter"></i>
+      </a>
 
-        <a href="" className="home__social-link" target="_blank">
-            <i className="fa-brands fa-behance"></i>
-        </a>
+      <a href="https://www.behance.net/" className="home__social-link" target="_blank" rel="noreferrer">
+        <i className="fa-brands fa-behance"></i>
+      </a>
 
-        <a href="" className="home__social-link" target="_blank">
-            <i className="fa-brands fa-dribbble"></i>
-        </a>
+      <a href="https://dribbble.com/" className="home__social-link" target="_blank" rel="noreferrer">
+        <i className="fa-brands fa-dribbble"></i>
+      </a>
 
-        <a href="" className="home__social-link" target="_blank">
-            <i className="fa-brands fa-pinterest"></i>
-        </a>
+      <a href="https://www.pinterest.com/" className="home__social-link" target="_blank" rel="noreferrer">
+        <i className="fa-brands fa-pinterest"></i>
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderSocials
+export default HeaderSocials;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,7 +2,7 @@
 import  "./home.css";
 import Me from "../../assets/avatar-1.svg";
 import HeaderSocials from "./HeaderSocials";
-
+import ScrollDown from "./ScrollDown";
 
 const Home = () => {
   return (
@@ -11,7 +11,12 @@ const Home = () => {
         <img src={Me} alt="" className="home__img" />
         <h1 className="home__name">Nurcan Topkara</h1>
         <span className="home__education">I am a Front-End developer</span>
+
     <HeaderSocials />
+
+    <a href="#contact" className="btn">Hire Me</a>
+
+    <ScrollDown />
     </div>
    </section>
   )
