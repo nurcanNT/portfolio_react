03444a169af0f640dfcf42d70d099e9d4c45ce4d
nurcanNT <nrcntpkra@gmail.com>
2023-05-26 17:57:28 +0300

    testimoniols update new

--- src/components/testimoniols/Testimoniols.jsx
+++ src/components/testimoniols/Testimoniols.jsx
@@ -1,11 +1,76 @@
 //import React from 'react'
 import "./testimoniols.css";
+import Image1 from "../../assets/avatar-1.svg";
+import Image2 from "../../assets/avatar-1.svg";
+import Image3 from "../../assets/avatar-1.svg";
+import Image4 from "../../assets/avatar-1.svg";
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+const data = [
+    {
+        id:1,
+        Image: Image1,
+        title:"John Doe",
+        subtitle: "Product designer at Dribble",
+        comment: "I enjoy working with the theme and learn so much. You guys make the process fun and interestingy. Good luck!",
+    },
+    {
+        id:2,
+        Image: Image2,
+        title:"John Doe",
+        subtitle: "Product designer at Dribble",
+        comment: "I enjoy working with the theme and learn so much. You guys make the process fun and interestingy. Good luck!",
+    },
+    {
+        id:3,
+        Image: Image3,
+        title:"John Doe",
+        subtitle: "Product designer at Dribble",
+        comment: "I enjoy working with the theme and learn so much. You guys make the process fun and interestingy. Good luck!",
+    },
+    {
+        id:4,
+        Image: Image4,
+        title:"John Doe",
+        subtitle: "Product designer at Dribble",
+        comment: "I enjoy working with the theme and learn so much. You guys make the process fun and interestingy. Good luck!",
+    },
+]
 
 const Testimoniols = () => {
   return (
-    <div>
-      testimoniols
-    </div>
+    <section className="testimonials container section">
+        <h2 className="section__title">Clients & Reviews</h2>
+
+        <Swiper className="testimonials__container grid"
+         modules={[ Pagination]}
+         spaceBetween={30}
+         slidesPerView={1}
+         loop={true}
+         grabCursor={true}
+         pagination={{ clickable: true }}
+         >
+            {data.map(({id, image, title, subtitle, comment}) => {
+                return (
+                    <SwiperSlide className="testimonial__item" key={id}>
+                        <div className="thumb">
+                            <img src={image} alt="" />
+                        </div>
+                        <h3 className="testimonials__title">{title}</h3>
+                        <span className="subtitle">{subtitle}</span>
+                        <div className="comment">{comment}</div>
+                    </SwiperSlide>
+                )
+            })}
+        </Swiper>
+    </section>
   )
 }
 
