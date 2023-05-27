6fc938b4278eb8d93fbb410399479a7a8876b8b1
nurcanNT <nrcntpkra@gmail.com>
2023-05-27 23:51:39 +0300

    blog page css

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,8 +1,8 @@
 //import React from 'react'
 import "./blog.css";
 import Image1 from "../../assets/blog-1.svg";
-import Image2 from "../../assets/blog-1.svg";
-import Image3 from "../../assets/blog-1.svg";
+import Image2 from "../../assets/blog-2.svg";
+import Image3 from "../../assets/blog-3.svg";
 
 const Blog = () => {
   return (
