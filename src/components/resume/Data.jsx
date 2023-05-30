87828a8f9e51d24aa9430601c4f17ef0859adc24
nurcanNT <nrcntpkra@gmail.com>
2023-05-30 23:35:41 +0300

    data update

--- src/components/resume/Data.jsx
+++ src/components/resume/Data.jsx
@@ -1,30 +1,7 @@
 //import React from 'react'
 
 const Data = [
-    {
-        id:1,
-        category: "education",
-        icon: "icon-graduation",
-        year: "2019 - present",
-        title: "Academic Degree",
-        desc: "Lorem ipsum color sit amet quo ei simul congue exerci ad nec admodum perfecto",
-    },
-    {
-        id:2,
-        category: "education",
-        icon: "icon-graduation",
-        year: "2019 - present",
-        title: "Bachelor's Degree",
-        desc: "Lorem ipsum color sit amet quo ei simul congue exerci ad nec admodum perfecto",
-    },
-    {
-        id:3,
-        category: "education",
-        icon: "icon-graduation",
-        year: "2019 - 2022",
-        title: "Academic Degree",
-        desc: "Lorem ipsum color sit amet quo ei simul congue exerci ad nec admodum perfecto",
-    },
+    
     {
         id:4,
         category: "education",
