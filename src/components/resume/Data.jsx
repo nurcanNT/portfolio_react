716a4a8eff6b611e0cb3021d937f37bec7a70c45
nurcanNT <nrcntpkra@gmail.com>
2023-06-07 23:34:27 +0300

    update

--- src/components/resume/Data.jsx
+++ src/components/resume/Data.jsx
@@ -10,5 +10,13 @@ const Data = [
         title: "Frontend Developer",
         desc: "HTML, CSS, JavaScript ile geliştirme yapılması, Tasarımsal hataların çözülmesi, Responsive konusunda çalışmaların yapılması, Müşteriden gelen feedbacklerin karşılanması",
     },
+    {
+        id:4,
+        category: "education",
+        icon: "icon-graduation",
+        year: "10.2020 - 12.2020",
+        title: "Web Developer",
+        desc: "JavaScript ve React teknolojisi ile geliştirme",
+    },
 ];
 export default Data
