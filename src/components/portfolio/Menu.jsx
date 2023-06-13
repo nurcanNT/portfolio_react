f4fec314026b232bdfb61b114113e28c875bb84b
nurcanNT <nrcntpkra@gmail.com>
2023-06-13 08:37:05 +0300

    portfolio image update

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -1,6 +1,9 @@
 //import React from 'react'
 import Work1 from '../../assets/nirvanna.png';
 import Work2 from '../../assets/talkshoes.png';
+import Work3 from '../../assets/newgamesimages.png';
+import Work4 from '../../assets/socialcloneimage.png';
+import Work5 from '../../assets/imgeartquake.png';
 
 const Menu = [
     {
@@ -12,21 +15,24 @@ const Menu = [
     {
         id: 2,
         title: "Nirvanna",
+        image: Work1,
         category: "E-commerce",
     },
     {
         id: 3,
         title: "Latest Earthquake News",
+        image:Work5,
         category: "JavaScript",
     },
     {
         id: 4,
         title: "Social Media Clone",
+        image: Work4,
         category: "JavaScipt",
     },
     {
         id: 5,
-        image: Work1,
+        image: Work3,
         title: "New Action Games",
         category: "Game",
     },
