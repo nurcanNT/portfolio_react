4066e75d0ec02ce85d99ec7f5b27ba75e2667a5a
nurcanNT <nrcntpkra@gmail.com>
2023-06-26 00:54:26 +0300

    update new

--- src/components/education/Data.jsx
+++ src/components/education/Data.jsx
@@ -19,24 +19,6 @@ const Data = [
         title2: "Python and Artificial Intelligence",
         desc: "3 ay boyunca süren 96 saatlik Python eğitiminin yanı sıra iş hayatına hazırlayan destekleyici eğitimler ve mentorluk desteği sunan donanımlı eğitim serüvenini bitirme projesini de yaparak başarılı bir şekilde mezun oldum.",
     },
-    {
-        id:4,
-        category: "education",
-        icon: "icon-graduation",
-        year: "06.2022 - 09.2022",
-        title: "Women in Tech Academy",
-        title2: "Python and Artificial Intelligence",
-        desc: "3 ay boyunca süren 96 saatlik Python eğitiminin yanı sıra iş hayatına hazırlayan destekleyici eğitimler ve mentorluk desteği sunan donanımlı eğitim serüvenini bitirme projesini de yaparak başarılı bir şekilde mezun oldum.",
-    },
-    {
-        id:4,
-        category: "education",
-        icon: "icon-graduation",
-        year: "06.2022 - 09.2022",
-        title: "Women in Tech Academy",
-        title2: "Python and Artificial Intelligence",
-        desc: "3 ay boyunca süren 96 saatlik Python eğitiminin yanı sıra iş hayatına hazırlayan destekleyici eğitimler ve mentorluk desteği sunan donanımlı eğitim serüvenini bitirme projesini de yaparak başarılı bir şekilde mezun oldum.",
-    },
     
 ];
 export default Data
