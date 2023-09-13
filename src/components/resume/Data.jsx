206cb5acedc7878974b4339628d8c9a75bba9ccc
nurcanNT <nrcntpkra@gmail.com>
2023-09-13 22:13:10 +0300

    test commit

--- src/components/resume/Data.jsx
+++ src/components/resume/Data.jsx
@@ -20,5 +20,14 @@ const Data = [
         title2: "Segmentify",
         desc: "HTML, CSS, JavaScript ile geliştirme yapılması, Tasarımsal hataların çözülmesi, Responsive konusunda çalışmaların yapılması, Müşteriden gelen feedbacklerin karşılanması",
     },
+    {
+        id:4,
+        category: "education",
+        icon: "icon-briefcase",
+        year: "10.2022 - 12.2022",
+        title: "Frontend Developer",
+        title2: "Segmentify",
+        desc: "HTML, CSS, JavaScript ile geliştirme yapılması, Tasarımsal hataların çözülmesi, Responsive konusunda çalışmaların yapılması, Müşteriden gelen feedbacklerin karşılanması",
+    },
 ];
 export default Data
